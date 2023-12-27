0e006dc855fe5913f5add22db6d272bd117fa0aa
Santosh Kumar <santoshkumar23kky@gmail.com>
2023-12-27 09:33:36 +0530

    addAnimation

--- Project2/script.js
+++ Project2/script.js
@@ -1,3 +1,38 @@
+gsap.registerPlugin(ScrollTrigger);
+
+// Using Locomotive Scroll from Locomotive https://github.com/locomotivemtl/locomotive-scroll
+
+const locoScroll = new LocomotiveScroll({
+  el: document.querySelector("#main"),
+  smooth: true
+});
+// each time Locomotive Scroll updates, tell ScrollTrigger to update too (sync positioning)
+locoScroll.on("scroll", ScrollTrigger.update);
+
+// tell ScrollTrigger to use these proxy methods for the "#main" element since Locomotive Scroll is hijacking things
+ScrollTrigger.scrollerProxy("#main", {
+  scrollTop(value) {
+    return arguments.length ? locoScroll.scrollTo(value, 0, 0) : locoScroll.scroll.instance.scroll.y;
+  }, // we don't have to define a scrollLeft because we're only scrolling vertically.
+  getBoundingClientRect() {
+    return {top: 0, left: 0, width: window.innerWidth, height: window.innerHeight};
+  },
+  // LocomotiveScroll handles things completely differently on mobile devices - it doesn't even transform the container at all! So to get the correct behavior and avoid jitters, we should pin things with position: fixed on mobile. We sense it by checking to see if there's a transform applied to the container (the LocomotiveScroll-controlled element).
+  pinType: document.querySelector("#main").style.transform ? "transform" : "fixed"
+});
+
+
+
+
+// each time the window updates, we should refresh ScrollTrigger and then update LocomotiveScroll. 
+ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
+
+// after everything is set up, refresh() ScrollTrigger and update LocomotiveScroll because padding may have been added for pinning, etc.
+ScrollTrigger.refresh();
+
+
+
+
 function cursorEffect(){
     var page1Content = document.querySelector("#page1-content")
 
@@ -27,6 +62,34 @@ page1Content.addEventListener("mouseleave",function(){
 
 cursorEffect()
 
+function page2Animation() {
+    gsap.from(".elem h1", {
+        y: 120,
+        stagger: 0.2,
+        duration: 1,
+        scrollTrigger: {
+            trigger: "#page2",
+            scroller: "body",
+            start: "top 47%",
+            end: "top 46%",
+            markrs: true,
+            scrub: 2
+        }
+    })
+}
+
+page2Animation()
 
 
-
+var tl = gsap.timeline();
+tl
+    .from("#page2-content", {
+        delay:2,
+        opacity: 0,
+        y:20
+    })
+    .from("#page2-main-content", {
+        delay:1,
+        opacity: 0,
+        y: 30
+    })
