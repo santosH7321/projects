143523e304293e2319cf7401017fbfe76d9ff5df
Santosh Kumar <santoshkumar23kky@gmail.com>
2023-12-29 00:01:43 +0530

    Swiper

--- Project2/script.js
+++ Project2/script.js
@@ -93,3 +93,16 @@ tl
         opacity: 0,
         y: 30
     })
+
+   function sliderAnimation(){
+    var swiper = new Swiper(".mySwiper", {
+        slidesPerView: 1,
+        spaceBetween: 30,
+        loop: true,
+        autoplay: {
+            delay: 2500,
+            disableOnInteraction: true,
+          },
+      });
+   }
+   sliderAnimation()
