9950e9c6efe3323584d249bb221fc58935b48984
Santosh Kumar <santoshkumar23kky@gmail.com>
2023-12-29 00:20:08 +0530

    Loader

--- Project2/script.js
+++ Project2/script.js
@@ -94,6 +94,7 @@ tl
         y: 30
     })
 
+
    function sliderAnimation(){
     var swiper = new Swiper(".mySwiper", {
         slidesPerView: 1,
@@ -106,3 +107,30 @@ tl
       });
    }
    sliderAnimation()
+
+ var tl = gsap.timeline()
+ tl.from("#loader h3",{
+    x: 40,
+    opacity: 0,
+    duration: 1,
+    stagger: .2,
+ })
+ tl.to("#loader h3",{
+    opacity: 0,
+    x: -10,
+    duration: 1,
+    stagger: 0.1,
+ })
+tl.to("#loader",{
+    opacity:0,
+})
+tl.from("#page1-content h1 span", {
+    y:100,
+    opacity: 0,
+    stagger: 0.1,
+    duration: -0.5
+})
+tl.to("#loader",{
+    display: "none"
+})
+
