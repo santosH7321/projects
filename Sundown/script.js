e7e4f495da61bc6ea650865b0a4959f855ac3f8e
Santosh Kumar <santoshkumar23kky@gmail.com>
2024-01-04 18:57:06 +0530

    page3

--- Sundown/script.js
+++ Sundown/script.js
@@ -2,3 +2,20 @@ const scroll = new LocomotiveScroll({
     el: document.querySelector('#main'),
     smooth: true
 });
+
+var element = document.querySelector("#elem-container")
+var fixed = document.querySelector("#fixed-image")
+element.addEventListener("mouseenter", function(){
+    fixed.style.display = "block";
+})
+element.addEventListener("mouseleave", function(){
+    fixed.style.display = "none";
+})
+
+var elems = document.querySelectorAll(".elem")
+elems.forEach(function(e){
+    e.addEventListener("mouseenter",function(){
+        var image = e.getAttribute("data-image")
+        fixed.style.backgroundImage = `url(${image})`
+    })
+})
