28298ac0e56c50a022b5aba9b730e8aa85a5c89b
Santosh Kumar <santoshkumar23kky@gmail.com>
2024-01-09 11:17:44 +0530

    question

---
+++ QuizApp/js/questions.js
@@ -0,0 +1,59 @@
+
+let questions = [
+    {
+        num: 1,
+        question: "What does HTML stand for?",
+        answer: "Hyper Text Markup Language",
+        options: [
+            "Hyper Text Mark Language",
+            "Hight Tech Language",
+            "Hyper Text Markup Language",
+            "Hyper Text Machine Language",
+        ]
+    },
+        {
+            num: 2,
+            question: "What does css stand for?",
+            answer: "Cascading Style Sheet",
+            options: [
+                "Common Style Sheet",
+                "Cascading Style Sheet",
+                "Color Style Sheet",
+                "Computer Style Sheet"
+            ]
+        },
+
+        {
+            num: 3,
+            question: "What does HTTP stand for?",
+            answer: "HyperText Transfer Protocol",
+            options: [
+                "HyperText Transfer Protocol Secure",
+                "HyperText Transfer Protocol",
+                "HyperTech Transfer Public Protocol",
+                "HyperText Transfer Public"
+            ]
+        },
+        {
+            num: 4,
+            question: "What does HTTPS stand for?",
+            answer: "HyperText Transfer Protocol Secure",
+            options: [
+                "HyperText Transfer Protocol Secure",
+                "HyperText Transfer Protocol",
+                "HyperTech Transfer Public Protocol",
+                "HyperText Transfer Public"
+            ]
+        },
+        {
+            num: 5,
+            question: "What does PHP stand for?",
+            answer: "Hypertext Preprocessor",
+            options: [
+                "HyperText Transfer Public",
+                "Power of HyperTech Public",
+                "Hypertext Preprocessor",
+                "Data Base"
+            ]
+        },
+]
