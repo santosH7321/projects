4a34f088586b02b518d87a1d83cb04031b134792
Santosh Kumar <santoshkumar23kky@gmail.com>
2024-01-16 19:15:15 +0530

    Loadder

--- modernLandingPage/script.js
+++ modernLandingPage/script.js
@@ -27,3 +27,14 @@ document.querySelector("#main")
     }
 })
 })
+
+function loaderAnimation() {
+    var loader = document.querySelector("#loader")
+    setTimeout(function () {
+        loader.style.top = "-100%"
+    }, 4200)
+}
+
+
+loaderAnimation()
+
