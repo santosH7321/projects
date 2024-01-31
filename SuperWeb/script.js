3fa0bb22134e736bd8a7d9e0a62d956422373cf8
Santosh Kumar <santoshkumar23kky@gmail.com>
2024-01-31 13:02:09 +0530

    page2AndFooter

--- SuperWeb/script.js
+++ SuperWeb/script.js
@@ -37,16 +37,23 @@ function loading() {
 }
 loading();
 
-const scroll = new LocomotiveScroll({
-  el: document.querySelector("#main"),
-  smooth: true,
-});
-
-var elems = document.querySelectorAll("#elem")
-var main = document.querySelector("#main")
-elems.forEach(function(ele){
-    ele.addEventListener("mouseenter", function(){
-      var bgImag = ele.getAttribute("data-img")
-      main.style.backgroundImage = `url(${bgImag})`
-    })
-})
+function loco(){
+  const scroll = new LocomotiveScroll({
+    el: document.querySelector("#main"),
+    smooth: true,
+  });
+  
+  document.querySelector("#footer h2").addEventListener("click", () => {
+    scroll.scrollTo(0)
+  })
+  
+  var elems = document.querySelectorAll("#elem")
+  var page2 = document.querySelector("#page2")
+  elems.forEach(function(ele){
+      ele.addEventListener("mouseenter", function(){
+        var bgImag = ele.getAttribute("data-img")
+        page2.style.backgroundImage = `url(${bgImag})`
+      })
+  })
+}
+loco();
