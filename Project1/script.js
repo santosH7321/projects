d0bd1d9e57992c4cdd3940c84c16e8ef2df614c0
Santosh Kumar <santoshkumar23kky@gmail.com>
2024-05-20 16:49:54 +0530

    tillNow

--- Project1/script.js
+++ Project1/script.js
@@ -67,7 +67,7 @@ ScrollReveal({
     delay: 200,
 
 })
-ScrollReveal().reveal(".home-content, .heading,{origin: 'top'}");
+ScrollReveal().reveal(".home-content, .heading",{origin: 'top'});
 
 ScrollReveal().reveal('.home-img img, .services-container, .portfolio-box, .testimonial-wrapper, .contact-form', {origin: 'botton'});
 
