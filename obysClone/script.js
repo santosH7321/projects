7841122b667282ab16b6389d0b8c4e27508852a8
Santosh Kumar <santoshkumar23kky@gmail.com>
2024-05-25 12:44:59 +0530

    loadderComplete

--- obysClone/script.js
+++ obysClone/script.js
@@ -1,12 +1,20 @@
 var tl = gsap.timeline();
-tl.from(".line h1", {
+tl.from(".line h1",{
   y: 150,
   stagger: 0.25,
   duration: 0.6,
-  delay: 0.5,
+  delay: 0.3,
 });
 
-tl.from("#line1-part1, .line h2", {
+tl.from(".line h4",{
+    y: 150,
+    stagger: 0.25,
+    duration: 0.6,
+    delay: 0.3,
+});
+
+
+tl.from("#line1-part1", {
   opacity: 0,
   onStart: function () {
     var h5timer = document.querySelector("#line1-part1 h5");
@@ -21,8 +29,25 @@ tl.from("#line1-part1, .line h2", {
   },
 });
 
+tl.to('.line h2', {
+    animationName: 'anime',
+    opacity: 1
+})
+
 tl.to("#loadder", {
   opacity: 0,
-  duration: 0.4,
-  delay: 4,
+  duration: 0.2,
+  delay: 3,
 });
+
+tl.from("#page1", {
+    delay: 0.2,
+    y: 1600,
+    opacity: 0,
+    duration: 0.5,
+    ease:Power4
+})
+
+tl.to("#loadder", {
+    display: "none",
+})
